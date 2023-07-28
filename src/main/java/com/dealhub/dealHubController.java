dc5c131bf3b8e4b81c5703f048a140625923ad5d
combo666 <shadat1124@gmail.com>
2023-07-29 00:30:10 +0600

    login page added

---
+++ src/main/java/com/dealhub/dealHubController.java
@@ -0,0 +1,26 @@
+package com.dealhub;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+public class dealHubController {
+
+    @FXML
+    ImageView loginImageView;
+    @FXML
+    TextField userId;
+    @FXML
+    TextField pass;
+    @FXML
+    Button loginBt;
+    @FXML
+    Button createAccBt;
+
+
+
+
+}
