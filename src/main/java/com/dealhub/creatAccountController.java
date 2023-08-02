1741102a097b26b13733a8d7c9a4bfa447b706fe
combo666 <shadat1124@gmail.com>
2023-08-03 01:44:19 +0600

    login and signup page update
    Searching , Multiple entry, Incorrect login or signup

--- src/main/java/com/dealhub/creatAccountController.java
+++ src/main/java/com/dealhub/creatAccountController.java
@@ -2,34 +2,29 @@ package com.dealhub;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
 
-import java.io.*;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Scanner;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Objects;
 
 
-class User {
-    String fName ;
-    String lName ;
-    String id ;
-    String pass ;
-    String cPass ;
 
-    User(String fName, String lNamem, String id, String pass, String cPass){
-        this.fName = fName;
-        this.lName = lNamem;
-        this.id = id;
-        this.pass = pass;
-        this.cPass = cPass;
-    }
-
-}
 
 public class creatAccountController {
+
+    public creatAccountController(){}
+
     @FXML
     private ImageView creatAccountIV;
     @FXML
@@ -47,7 +42,21 @@ public class creatAccountController {
     @FXML
     private Button loginBt;
 
+    Image photo = new Image(Objects.requireNonNull(getClass().getResourceAsStream("creatAccountIncorrectPage.png")));
 
+    @FXML
+    public void loginAcc(ActionEvent event) throws IOException {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(loginApplication.class.getResource("login.fxml"));
+            Parent root = (Parent) fxmlLoader.load();
+            Stage stage = new Stage();
+            stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
+            stage.setScene(new Scene(root));
+            stage.show();
+
+        }catch (Exception ignored){}
+
+    }
 
 
     @FXML
@@ -64,19 +73,54 @@ public class creatAccountController {
         String pass = passwordTF.getText();
         String cPass = confirmPasswordTF.getText();
 
+        boolean isNumeric = true;
         User us = new User(fName , lName , id , pass , cPass);
 
-        System.out.printf(" "+us.fName);
+        try{
+            Double d = Double.parseDouble(id);
+        }catch (Exception e){
+            isNumeric = false;
+        }
 
+        if(!isNumeric){
+            creatAccountIV.setImage(photo);
+        }else{
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.append(id).append(",").append(fName).append(",").append(lName).append(",").append(pass).append(",").append(cPass).append("\n");
 
-//        try {
-//            FileOutputStream fos = new FileOutputStream("data.txt");
-//            ObjectOutputStream oos = new ObjectOutputStream(fos);
-//            oos.writeObject(us);
-//            fos.close();
-//            oos.close();
-//        }catch(Exception ignored){}
+            try(FileWriter writer = new FileWriter("data.csv", true);) {
+                writer.write(stringBuilder.toString());
+                writer.close();
+            }catch(IOException ignored){}
+        }
 
+    }
 
+}
+
+class User {
+    String fName ;
+    String lName ;
+    String id ;
+    String pass ;
+    String cPass ;
+
+    User(String fName, String lNamem, String id, String pass, String cPass){
+        this.fName = fName;
+        this.lName = lNamem;
+        this.id = id;
+        this.pass = pass;
+        this.cPass = cPass;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "fName='" + fName + '\'' +
+                ", lName='" + lName + '\'' +
+                ", id='" + id + '\'' +
+                ", pass='" + pass + '\'' +
+                ", cPass='" + cPass + '\'' +
+                '}';
     }
 }
--- src/main/java/com/dealhub/loginController.java
+++ src/main/java/com/dealhub/loginController.java
@@ -10,13 +10,17 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
+import java.io.File;
 import java.io.IOException;
 import java.net.URL;
+import java.util.Objects;
 import java.util.ResourceBundle;
+import java.util.Scanner;
 
 public class loginController implements Initializable {
 
@@ -33,12 +37,15 @@ public class loginController implements Initializable {
     private Button loginBt;
     @FXML
     private Button createAccBt;
+
+    private FileChooser fileChooser;
     private Scene scene;
     private Parent root;
 
+    Image photo = new Image(Objects.requireNonNull(getClass().getResourceAsStream("loginIncorrectIDPass.png")));
+
     @FXML
     public void creatAcc(ActionEvent event) throws IOException {
-
         try {
             FXMLLoader fxmlLoader = new FXMLLoader(loginApplication.class.getResource("creatAccount.fxml"));
             Parent root = (Parent) fxmlLoader.load();
@@ -50,17 +57,40 @@ public class loginController implements Initializable {
         }catch (Exception ignored){}
 
     }
+
     @FXML
     public void logIn(ActionEvent event) throws IOException {
         checkLogIn();
     }
+
+    String searchId = "", searchPass = "" , garbage1 = "", garbage2 = "", garbage3 = "", garbage4 = "";
+
     @FXML
     private void checkLogIn() throws IOException{
         String id=uIdTF.getText();
         String pass = passTF.getText();
 
-        if (id != null && pass != null){
+        boolean found = false;
 
+        Scanner x = new Scanner(new File("data.csv"));
+        x.useDelimiter("[,\n]");
+
+        while (x.hasNext() && !found){
+            searchId = x.next();
+            garbage1 = x.next();
+            garbage2 = x.next();
+            garbage3 = x.next();
+            garbage4 = x.next();
+
+            if(searchId.equals(pass)){
+                found = true;
+                System.out.println(searchId);
+            }
+        }
+
+
+        if (id == null && pass == null || !found){
+            loginImageView.setImage(photo);
         }
 
 
