be52a568dece440db3a2145c29e6a45ad16e5c3e
combo666 <shadat1124@gmail.com>
2023-08-04 02:37:55 +0600

    Home page testing

--- src/main/java/com/dealhub/homeApplication.java
+++ src/main/java/com/dealhub/homeApplication.java
@@ -1,7 +1,11 @@
 package com.dealhub;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class homeApplication extends Application {
 
@@ -10,7 +14,11 @@ public class homeApplication extends Application {
     }
 
     @Override
-    public void start(Stage primaryStage) {
-
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(loginApplication.class.getResource("home.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 1920, 1080);
+        stage.setTitle("DealHub");
+        stage.setScene(scene);
+        stage.show();
     }
 }
