b5bf299b2f11057c0f7a6363a8b1f0d499126345
Tanvin Chowdhury <tanvin.chowdhury999@gmail.com>
2023-08-06 22:52:51 +0600

    added homepage card fxml file

--- src/main/java/com/dealhub/homeController.java
+++ src/main/java/com/dealhub/homeController.java
@@ -1,4 +1,26 @@
 package com.dealhub;
 
-public class homeController {
+import javafx.application.Application;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class homeController extends Application {
+    @FXML
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(loginApplication.class.getResource("home.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 1920, 1080);
+        stage.setTitle("DealHub");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
 }
+
+
