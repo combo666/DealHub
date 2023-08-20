17ab4eefa145d971077224a7490db34fbb358a75
Tanvin Chowdhury <tanvin.chowdhury999@gmail.com>
2023-08-19 23:56:03 -0700

    auctionRoom and adminAuction dlt btn
    left row wise name and all other things

--- src/main/java/com/dealhub/adminAuctionsController.java
+++ src/main/java/com/dealhub/adminAuctionsController.java
@@ -11,6 +11,9 @@ import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.net.URL;
+import java.sql.*;
+import java.util.ResourceBundle;
 
 public class adminAuctionsController {
     @FXML
@@ -37,95 +40,158 @@ public class adminAuctionsController {
     @FXML
     private Button userManagementBtn;
 
+
     @FXML
     public void setAdminPostBtn(ActionEvent event) throws IOException {
         try {
             FXMLLoader fxmlLoader = new FXMLLoader(loginApplication.class.getResource("login.fxml"));
             Parent root = (Parent) fxmlLoader.load();
             Stage stage = new Stage();
-            stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
+            stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
             stage.setScene(new Scene(root));
             stage.show();
 
-        }catch (Exception ignored){}
+        } catch (Exception ignored) {
+        }
 
     }
+
     @FXML
     public void setBidManagementBtn(ActionEvent event) throws IOException {
         try {
             FXMLLoader fxmlLoader = new FXMLLoader(loginApplication.class.getResource("login.fxml"));
             Parent root = (Parent) fxmlLoader.load();
             Stage stage = new Stage();
-            stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
+            stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
             stage.setScene(new Scene(root));
             stage.show();
 
-        }catch (Exception ignored){}
+        } catch (Exception ignored) {
+        }
 
     }
+
     @FXML
     public void setDashboardBtn(ActionEvent event) throws IOException {
         try {
             FXMLLoader fxmlLoader = new FXMLLoader(loginApplication.class.getResource("login.fxml"));
             Parent root = (Parent) fxmlLoader.load();
             Stage stage = new Stage();
-            stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
+            stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
             stage.setScene(new Scene(root));
             stage.show();
 
-        }catch (Exception ignored){}
+        } catch (Exception ignored) {
+        }
 
     }
+
     @FXML
     public void setEndAuctionBtn(ActionEvent event) throws IOException {
-        try {
-            FXMLLoader fxmlLoader = new FXMLLoader(loginApplication.class.getResource("login.fxml"));
-            Parent root = (Parent) fxmlLoader.load();
-            Stage stage = new Stage();
-            stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
-            stage.setScene(new Scene(root));
-            stage.show();
+        Connection connection = null;
+        String jdbcUrl = "jdbc:mysql://localhost:3306/dealhub";
+        String username = "root";
+        String password = "";
 
-        }catch (Exception ignored){}
+        try {
+            connection = DriverManager.getConnection(jdbcUrl, username, password);
+            System.out.println("Connected to the database!");
+        } catch (
+                SQLException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            assert connection != null;
+            Statement statement = connection.createStatement();
+            String sqlQuery = "SELECT * FROM uploadproducts";
+            ResultSet resultSet = statement.executeQuery(sqlQuery);
+
+            while (resultSet.next()) {
+                String searchID = resultSet.getString("id");
+                if (searchID.equals(endAuctionTextField.getText())) {
+                    System.out.println("Found");
+                    String sql = "DELETE FROM `uploadproducts` WHERE id=?";
+
+                   PreparedStatement preparedStatement = connection.prepareStatement(sql);
+                    preparedStatement.setString(1, endAuctionTextField.getText());
+                    int rowsAffected = preparedStatement.executeUpdate();
+                    System.out.println(rowsAffected + " row(s) inserted.");
+                    preparedStatement.close();
+
+                    try {
+                        FXMLLoader fxmlLoader = new FXMLLoader(loginApplication.class.getResource("login.fxml"));
+                        Parent root = (Parent) fxmlLoader.load();
+                        Stage stage = new Stage();
+                        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+                        stage.setScene(new Scene(root));
+                        stage.show();
+
+                    } catch (Exception ignored) {
+                    }
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            if (connection != null) {
+                connection.close();
+                System.out.println("Connection closed.");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
 
     }
+
     @FXML
     public void setLogoutBtn(ActionEvent event) throws IOException {
         try {
             FXMLLoader fxmlLoader = new FXMLLoader(loginApplication.class.getResource("login.fxml"));
             Parent root = (Parent) fxmlLoader.load();
             Stage stage = new Stage();
-            stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
+            stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
             stage.setScene(new Scene(root));
             stage.show();
 
-        }catch (Exception ignored){}
+        } catch (Exception ignored) {
+        }
 
     }
+
     @FXML
     public void setRoomManagementBtn(ActionEvent event) throws IOException {
         try {
             FXMLLoader fxmlLoader = new FXMLLoader(loginApplication.class.getResource("login.fxml"));
             Parent root = (Parent) fxmlLoader.load();
             Stage stage = new Stage();
-            stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
+            stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
             stage.setScene(new Scene(root));
             stage.show();
 
-        }catch (Exception ignored){}
+        } catch (Exception ignored) {
+        }
 
     }
+
     @FXML
     public void setUserManagementBtn(ActionEvent event) throws IOException {
         try {
             FXMLLoader fxmlLoader = new FXMLLoader(loginApplication.class.getResource("login.fxml"));
             Parent root = (Parent) fxmlLoader.load();
             Stage stage = new Stage();
-            stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
+            stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
             stage.setScene(new Scene(root));
             stage.show();
 
-        }catch (Exception ignored){}
+        } catch (Exception ignored) {
+        }
 
+    }
+
+
+    public void initialize(URL url, ResourceBundle resourceBundle) {
     }
 }
--- src/main/java/com/dealhub/adminRoomManagementController.java
+++ src/main/java/com/dealhub/adminRoomManagementController.java
@@ -11,6 +11,7 @@ import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.sql.*;
 
 public class adminRoomManagementController {
     @FXML
@@ -104,6 +105,62 @@ public class adminRoomManagementController {
     }
     @FXML
     public void setDeleteBtn(ActionEvent event) throws IOException {
+
+        Connection connection = null;
+        String jdbcUrl = "jdbc:mysql://localhost:3306/dealhub";
+        String username = "root";
+        String password = "";
+
+        try {
+            connection = DriverManager.getConnection(jdbcUrl, username, password);
+            System.out.println("Connected to the database!");
+        } catch (
+                SQLException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            assert connection != null;
+            Statement statement = connection.createStatement();
+            String sqlQuery = "SELECT * FROM uploadproducts";
+            ResultSet resultSet = statement.executeQuery(sqlQuery);
+
+            while (resultSet.next()) {
+                String searchID = resultSet.getString("id");
+                if (searchID.equals(roomTxtField.getText())) {
+                    System.out.println("Found");
+                    String sql = "DELETE FROM `uploadproducts` WHERE id=?";
+
+                    PreparedStatement preparedStatement = connection.prepareStatement(sql);
+                    preparedStatement.setString(1, roomTxtField.getText());
+                    int rowsAffected = preparedStatement.executeUpdate();
+                    System.out.println(rowsAffected + " row(s) inserted.");
+                    preparedStatement.close();
+
+                    try {
+                        FXMLLoader fxmlLoader = new FXMLLoader(loginApplication.class.getResource("login.fxml"));
+                        Parent root = (Parent) fxmlLoader.load();
+                        Stage stage = new Stage();
+                        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+                        stage.setScene(new Scene(root));
+                        stage.show();
+
+                    } catch (Exception ignored) {
+                    }
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            if (connection != null) {
+                connection.close();
+                System.out.println("Connection closed.");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
         try {
             FXMLLoader fxmlLoader = new FXMLLoader(loginApplication.class.getResource("login.fxml"));
             Parent root = (Parent) fxmlLoader.load();
