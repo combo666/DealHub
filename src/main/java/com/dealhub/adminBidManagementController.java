d771f60017dd5673dbe0523e3641536e79bb4993
Tanvin Chowdhury <tanvin.chowdhury999@gmail.com>
2023-08-24 22:12:11 -0700

    adminBidManagement done

--- src/main/java/com/dealhub/adminBidManagementController.java
+++ src/main/java/com/dealhub/adminBidManagementController.java
@@ -3,16 +3,24 @@ package com.dealhub;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.net.URL;
+import java.sql.*;
+import java.util.ResourceBundle;
 
-public class adminBidManagementController {
+public class adminBidManagementController implements Initializable {
     @FXML
     private Button adminPostBtn;
 
@@ -38,18 +46,23 @@ public class adminBidManagementController {
     private Button userManagementBtn;
 
     @FXML
+    VBox vBox = new VBox();
+
+    @FXML
     public void setAdminPostBtn(ActionEvent event) throws IOException {
         try {
             FXMLLoader fxmlLoader = new FXMLLoader(loginApplication.class.getResource("adminPost.fxml"));
             Parent root = (Parent) fxmlLoader.load();
             Stage stage = new Stage();
-            stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
+            stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
             stage.setScene(new Scene(root));
             stage.show();
 
-        }catch (Exception ignored){}
+        } catch (Exception ignored) {
+        }
 
     }
+
     @FXML
     public void setLogoutBtn(ActionEvent event) throws IOException {
         try {
@@ -57,11 +70,12 @@ public class adminBidManagementController {
             FXMLLoader fxmlLoader = new FXMLLoader(loginApplication.class.getResource("home.fxml"));
             Parent root = (Parent) fxmlLoader.load();
             Stage stage = new Stage();
-            stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
+            stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
             stage.setScene(new Scene(root));
             stage.show();
 
-        }catch (Exception ignored){}
+        } catch (Exception ignored) {
+        }
 
     }
 
@@ -71,58 +85,187 @@ public class adminBidManagementController {
             FXMLLoader fxmlLoader = new FXMLLoader(loginApplication.class.getResource("adminDashboard.fxml"));
             Parent root = (Parent) fxmlLoader.load();
             Stage stage = new Stage();
-            stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
+            stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
             stage.setScene(new Scene(root));
             stage.show();
 
-        }catch (Exception ignored){}
+        } catch (Exception ignored) {
+        }
 
     }
+
     @FXML
     public void setAuctionManagementBtn(ActionEvent event) throws IOException {
         try {
             FXMLLoader fxmlLoader = new FXMLLoader(loginApplication.class.getResource("adminAuctions.fxml"));
             Parent root = (Parent) fxmlLoader.load();
             Stage stage = new Stage();
-            stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
+            stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
             stage.setScene(new Scene(root));
             stage.show();
 
-        }catch (Exception ignored){}
+        } catch (Exception ignored) {
+        }
 
     }
+
     @FXML
     public void setUserManagementBtn(ActionEvent event) throws IOException {
         try {
             FXMLLoader fxmlLoader = new FXMLLoader(loginApplication.class.getResource("adminUserManagement.fxml"));
             Parent root = (Parent) fxmlLoader.load();
             Stage stage = new Stage();
-            stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
+            stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
             stage.setScene(new Scene(root));
             stage.show();
 
-        }catch (Exception ignored){}
+        } catch (Exception ignored) {
+        }
 
     }
+
     @FXML
     public void setRoomManagementBtn(ActionEvent event) throws IOException {
         try {
             FXMLLoader fxmlLoader = new FXMLLoader(loginApplication.class.getResource("adminRoomManagement.fxml"));
             Parent root = (Parent) fxmlLoader.load();
             Stage stage = new Stage();
-            stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
+            stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
             stage.setScene(new Scene(root));
             stage.show();
 
-        }catch (Exception ignored){}
+        } catch (Exception ignored) {
+        }
 
     }
+
     @FXML
     public void setEndAuctionBtn(ActionEvent event) throws IOException {
+        Connection connection = null;
+        String jdbcUrl = "jdbc:mysql://localhost:3306/dealhub";
+        String username = "root";
+        String password = "";
 
+        try {
+            connection = DriverManager.getConnection(jdbcUrl, username, password);
+            System.out.println("Connected to the database!");
+        } catch (
+                SQLException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            assert connection != null;
+            Statement statement = connection.createStatement();
+            String sqlQuery = "SELECT * FROM `uploadproducts` WHERE 1";
+            ResultSet resultSet = statement.executeQuery(sqlQuery);
+
+            while (resultSet.next()) {
+                String searchID = resultSet.getString("id");
+                if (searchID.equals(endAuctionTF.getText())) {
+                    System.out.println("Found");
+                    String sql = "DELETE FROM `uploadproducts` WHERE id=?";
+
+                    PreparedStatement preparedStatement = connection.prepareStatement(sql);
+                    preparedStatement.setString(1, endAuctionTF.getText());
+                    int rowsAffected = preparedStatement.executeUpdate();
+                    System.out.println(rowsAffected + " row(s) inserted.");
+                    preparedStatement.close();
+
+                    try {
+                        FXMLLoader fxmlLoader = new FXMLLoader(loginApplication.class.getResource("login.fxml"));
+                        Parent root = (Parent) fxmlLoader.load();
+                        Stage stage = new Stage();
+                        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+                        stage.setScene(new Scene(root));
+                        stage.show();
+
+                    } catch (Exception ignored) {
+                    }
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            if (connection != null) {
+                connection.close();
+                System.out.println("Connection closed.");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
 
     }
 
 
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        Connection connection = null;
+        String jdbcUrl = "jdbc:mysql://localhost:3306/dealhub";
+        String username = "root";
+        String password = "";
 
+        try {
+            assert connection != null;
+            connection = DriverManager.getConnection(jdbcUrl, username, password);
+            System.out.println("Connected to the database!");
+        } catch (
+                SQLException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            assert connection != null;
+            Statement statement = connection.createStatement();
+            String sqlQuery = "SELECT * FROM `uploadproducts` WHERE 1";
+            ResultSet resultSet = statement.executeQuery(sqlQuery);
+
+            while (resultSet.next()) {
+                String id = resultSet.getString("id");
+                String bid = resultSet.getString("current_bid");
+                String remTime = resultSet.getString("end_time");
+
+                System.out.println("found");
+
+                AnchorPane anchorPane = new AnchorPane();
+                anchorPane.setMaxSize(850, 25);
+
+                Label idLebel = new Label(id);
+                idLebel.setMaxSize(325, 25);
+                idLebel.setLayoutX(0);
+                idLebel.setFont(Font.font("Arial", 15));
+
+               Label bidLebel = new Label(bid);
+                bidLebel.setMaxSize(310, 25);
+                bidLebel.setLayoutX(325);
+                bidLebel.setFont(Font.font("Arial",15));
+
+                Label timeLebel = new Label(remTime);
+                timeLebel.setMaxSize(215, 25);
+                timeLebel.setLayoutX(643);
+                timeLebel.setFont(Font.font("Arial",15));
+
+                anchorPane.getChildren().addAll(idLebel, bidLebel, timeLebel);
+                vBox.getChildren().add(anchorPane);
+
+                System.out.println("found");
+
+
+            }
+        } catch (SQLException e) {
+        }
+
+
+        try {
+            if (connection != null) {
+                connection.close();
+                System.out.println("Connection closed.");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 }
+
